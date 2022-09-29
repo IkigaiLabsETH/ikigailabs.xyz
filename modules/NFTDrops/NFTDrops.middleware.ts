3271462b57c025c8c45941493fbac171f3df77fc
Roy <hi@royscheeren.com>
2022-09-29 20:12:23 +0200

    fix: unused

--- modules/NFTDrops/NFTDrops.middleware.ts
+++ modules/NFTDrops/NFTDrops.middleware.ts
@@ -1,10 +1,10 @@
 import { Action, ListenerEffectAPI } from '@reduxjs/toolkit'
-import { NFTDrops } from '../../common/config'
+// import { NFTDrops } from '../../common/config'
 
 import { AppDispatch, RootState } from '../../common/redux/store'
 import { Web3 } from '../../common/web3'
 import { appInit } from '../App/app.reducer'
-import { fetchNFTDrops } from './NFTDrops.slice'
+// import { fetchNFTDrops } from './NFTDrops.slice'
 
 export const middleware = (web3: Web3) => ({
   actionCreator: appInit,
