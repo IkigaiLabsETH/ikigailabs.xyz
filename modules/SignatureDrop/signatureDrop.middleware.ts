924eebbfeea46c9149a920601781ff54108774c0
Roy <hi@royscheeren.com>
2022-09-30 21:55:55 +0200

    refactor: reinstate nfts

--- modules/SignatureDrop/signatureDrop.middleware.ts
+++ modules/SignatureDrop/signatureDrop.middleware.ts
@@ -15,7 +15,7 @@ export const middleware = {
   actionCreator: fetchSignatureDrop,
   effect: (action: PayloadAction<{ contract: string }>, listenerApi: ListenerEffectAPI<RootState, AppDispatch>) => {
     listenerApi.dispatch(fetchSignatureDropMetadata(prop('payload')(action)))
-    // listenerApi.dispatch(fetchSignatureDropNFTs(prop('payload')(action)))
+    listenerApi.dispatch(fetchSignatureDropNFTs(prop('payload')(action)))
     listenerApi.dispatch(fetchSignatureDropClaimedSupply(prop('payload')(action)))
     listenerApi.dispatch(fetchSignatureDropUnclaimedSupply(prop('payload')(action)))
     listenerApi.dispatch(fetchSignatureDropClaimConditions(prop('payload')(action)))
