f3cdce167af535d14e20f4a7a8a22f8b4d6bc427
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2024-06-10 14:12:10 +0200

    MarketStats Footer

--- modules/Footer/Footer.tsx
+++ modules/Footer/Footer.tsx
@@ -6,25 +6,7 @@ export const Footer: FC = () => (
     <div className="flex items-center justify-center text-16 font-bold tracking-tighter leading-none">
       IKIGAI LABS XYZ
     </div>
-    <div className="flex items-center justify-center">
-      <ul className="flex flex-row items-center">
-        <li className="p-4 w-16">
-          <a href="https://discord.gg/aAuvja73Cg">
-            <Image src="/assets/images/discord.svg" alt="discord" width={41} height={32} />
-          </a>
-        </li>
-        <li className="p-4 w-16">
-          <a href="https://twitter.com/ikigailabsETH">
-            <Image src="/assets/images/twitter.svg" alt="discord" width={32} height={32} />
-          </a>
-        </li>
-        {/* <li className="p-4 w-16">
-          <a href="https://instagram.com" className="flex justify-center items-center">
-            <Image src="/assets/images/instagram.svg" alt="discord" width={32} height={32} />
-          </a>
-        </li> */}
-      </ul>
-    </div>
+    <MarketStats />
     <div className="flex items-center justify-end">
       <a href="#" title="" className="flex items-center">
         <Image src="/assets/images/IKIGAI_LABS_logo.svg" alt="logo" width="32" height="32" />
