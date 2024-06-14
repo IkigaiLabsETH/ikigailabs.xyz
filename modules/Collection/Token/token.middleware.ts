4ab9c1f1425cab3645b59c145c81f65c31969362
Roy Scheeren <hi@royscheeren.com>
2024-06-14 16:37:51 +0200

    feat: token serversideprops for head info
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- modules/Collection/Token/token.middleware.ts
+++ modules/Collection/Token/token.middleware.ts
@@ -1,5 +1,5 @@
-import { ListenerEffectAPI, PayloadAction, isFulfilled } from '@reduxjs/toolkit'
-import { omit, pathOr } from 'ramda'
+import { ListenerEffectAPI, PayloadAction } from '@reduxjs/toolkit'
+import { pathOr } from 'ramda'
 
 import { AppDispatch, RootState } from '../../../common/redux/store'
 import { fetchCollectionToken } from './token.actions'
