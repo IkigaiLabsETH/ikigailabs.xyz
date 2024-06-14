507d238129b6d2d1e45786d8bedb4e52408f788a
Roy Scheeren <hi@royscheeren.com>
2024-06-14 14:08:49 +0200

    fix: meta image url
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- common/constants/site-data.ts
+++ common/constants/site-data.ts
@@ -1,4 +1,5 @@
 export const SITE_TITLE = 'Ikigai Labs - Curated Art Explorer'
 export const SITE_DESCRIPTION = 'Explore the world’s most exceptional cultural artifacts with the Ikigai Labs XYZ Curated Art Explorer. Since 2017, we’ve meticulously selected the top thousand photography, AI, and generative art collections, highlighting unmatched artistic innovation. Dive into the unchained creativity of diverse artists and experience a curated array of extraordinary art.'
 export const SITE_URL = 'https://ikigailabs.xyz'
-export const SITE_LOGO_PATH = '/assets/images/IKIGAI_LABS_logo.svg'
+export const SITE_LOGO_PATH = `${SITE_URL}/assets/images/IKIGAI_LABS_logo.svg`
+
