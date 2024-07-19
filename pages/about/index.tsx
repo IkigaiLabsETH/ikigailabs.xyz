97652cf4080fd656a63503e49ac28d0c52f92636
Roy Scheeren <hi@royscheeren.com>
2024-07-19 12:12:54 +0200

    feat: cosmetic fixes about page
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -37,10 +37,10 @@ const About: FC = () => (
     </Head>
     <main className="max-w-screen-2xl w-full">
       <div className="flex relative flex-col text-lg mt-48 max-w-3xl mx-auto p-8">
-        <h1 className="text-[2rem] md:text-[4rem] lg:text-[6rem] leading-none font-bold mb-4 tracking-tight boska break-words">
+        <h1 className="text-[3rem] md:text-[4rem] lg:text-[6rem] leading-none font-bold mb-4 tracking-tight boska break-words">
           About
         </h1>
-        <h2 className="mt-20">Empowering Creators</h2>
+        <h2 className="mt-10 md:mt-20 text-[2rem] md:text-[3rem]">Empowering Creators</h2>
         <p>
           At Ikigai Labs, we believe that every artist deserves a platform where their creativity can thrive without
           boundaries. Our mission goes beyond simple curation; we empower creators by integrating cutting-edge
@@ -49,7 +49,7 @@ const About: FC = () => (
           behind it. Here, artists find their ikigai—a convergence of passion, innovation, and technology.
         </p>
 
-        <h2 className="mt-20">Innovative Curation</h2>
+        <h2 className="mt-10 md:mt-20 text-[2rem] md:text-[3rem]">Innovative Curation</h2>
         <p>
           Curation at Ikigai Labs is a transformative experience. Our curators are more than selectors; they are the
           architects of an ever-evolving art ecosystem. By incorporating insights and expertise, they continually infuse
@@ -61,7 +61,7 @@ const About: FC = () => (
           experiences that transcend the digital marketplace.
         </p>
 
-        <h2 className="mt-20">Dynamic Collecting</h2>
+        <h2 className="mt-10 md:mt-20 text-[2rem] md:text-[3rem]">Dynamic Collecting</h2>
         <p>
           We recognize collectors as the lifeblood of the art world. At Ikigai Labs, we extend beyond traditional
           collecting by creating a meticulously curated environment filled with extraordinary digital art and immersive
@@ -70,14 +70,14 @@ const About: FC = () => (
           community of passionate art enthusiasts.
         </p>
 
-        <h2 className="mt-20">Revolutionizing Authenticity with TRACE</h2>
+        <h2 className="mt-10 md:mt-20 text-[2rem] md:text-[3rem]">Revolutionizing Authenticity with TRACE</h2>
         <p>
           Authenticity and provenance are paramount in the digital art space. Our T.R.A.C.E. (Transparent, Reliable,
           Authentic, Certified, and Encrypted) system revolutionizes this aspect by embedding secure chips in artworks,
           paired with digital certificates of authenticity. This advanced technology, developed by Transient Labs,
           guarantees the integrity and value of every piece on our platform.
         </p>
-        <h2 className="mt-20">Our Vision</h2>
+        <h2 className="mt-10 md:mt-20 text-[2rem] md:text-[3rem]">Our Vision</h2>
         <p>
           Inspired by the Japanese concept of &apos;ikigai&apos;, which represents the balance of passion, talent, societal
           contribution, and livelihood, Ikigai Labs aims to create a sustainable ecosystem that nurtures this harmony.
@@ -85,7 +85,7 @@ const About: FC = () => (
           creativity. We are committed to fostering a community where art and innovation intersect, creating lasting
           value for creators, curators, and collectors.
         </p>
-        <h2 className="mt-20">Join us</h2>
+        <h2 className="mt-10 md:mt-20 text-[2rem] md:text-[3rem]">Join us</h2>
         <p>
           Whether you are an artist looking to share your unique vision, a curator seeking to influence the art
           landscape, or a collector passionate about digital art, we provide the tools, technology, and community to
