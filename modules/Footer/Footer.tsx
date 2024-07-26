0f4e5815cd1257e50f3c444793934ecbbcd7d326
Roy Scheeren <hi@royscheeren.com>
2024-07-26 14:14:35 +0200

    feat: update footer
    Signed-off-by: Roy Scheeren <hi@royscheeren.com>

--- modules/Footer/Footer.tsx
+++ modules/Footer/Footer.tsx
@@ -4,10 +4,9 @@ import { MarketStats } from '../MarketStats'
 import Link from 'next/link'
 
 export const Footer: FC = () => (
-  <div className="flex flex-row px-4 mx-auto max-w-screen-2xl sm:px-6 lg:px-8 justify-between items-center w-full py-8">
-    <div className="flex items-left justify-center text-16 font-bold tracking-tighter leading-none flex-col">
-      &copy; {new Date().getFullYear()} IKIGAI LABS XYZ
-      <ul className='font-normal mt-6'>
+  <div className="flex flex-row px-4 mx-auto max-w-screen-2xl sm:px-6 lg:px-8 justify-between w-full py-8">
+    <div className="flex items-left justify-center font-bold flex-col">
+      <ul className='font-normal'>
         <li className='p-1'>
           <Link href="/disclosures">
             Disclosures
@@ -24,18 +23,18 @@ export const Footer: FC = () => (
           </Link>
         </li>
         <li className='p-1'>
-          <Link href="/ambassadors">
-            Ambassadors
+          <Link href="/about">
+            About Ikigai
           </Link>
         </li>
       </ul>
 
     </div>
     <MarketStats />
-    <div className="flex items-center justify-end">
-      <a href="#" title="" className="flex items-center">
-        <Image src="/assets/images/IKIGAI_LABS_logo.svg" alt="logo" width="32" height="32" />
-      </a>
+    <div className="flex items-start justify-start">
+      <Link href="/" title="" className="flex items-center">
+      <span className='pr-2 mr-1.5 border-r '>&copy; {new Date().getFullYear()} IKIGAI LABS XYZ </span><Image src="/assets/images/IKIGAI_LABS_logo.svg" alt="logo" width="16" height="16" />
+      </Link>
     </div>
   </div>
 )
