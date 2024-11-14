7d4c9b046329580a3ebe7df0af932e32e5b82203
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2024-11-14 20:52:24 +0100

    Update GemsOnTheFloor.tsx

--- modules/GemsOnTheFloor/GemsOnTheFloor.tsx
+++ modules/GemsOnTheFloor/GemsOnTheFloor.tsx
@@ -72,7 +72,7 @@ export const GemsOnTheFloor: FC = () => {
       </div>
       <div className="w-full !text-black max-w-80 md:max-w-screen-md lg:max-w-screen-lg 2xl:max-w-screen-2xl mt-3">
         <h1 className="ml-3 text-4xl md:text-6xl lg:text-8xl text-black translate-y-10 md:translate-y-11">
-          Photography
+          a ppr oc he extended
         </h1>
         <div className="bg-white">
           {equals(photographyFloorsStatus, QueryStatus.pending) ? (
