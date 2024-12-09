7a95e9f0693b90bb61609e0eb6993d47c4b69b59
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2024-12-09 19:26:10 +0100

    Update index.tsx

--- pages/photography/index.tsx
+++ pages/photography/index.tsx
@@ -1,57 +1,51 @@
-import React, { FC, useEffect } from 'react'
-import { useAppDispatch, useAppSelector } from '../../common/redux/store'
-import {
-  GEMS_ON_THE_FLOOR_PHOTOGRAPHY_COLLECTION_SET_ID,
-} from '../../common/config'
-import { collectionsApi, selectCollectionFloorsByCollectionSetId } from '../Collections/collections.api'
-import { Network } from '../../common/types'
-import { equals } from 'ramda'
-import { QueryStatus } from '@reduxjs/toolkit/query'
-import { SkeletonLoader } from '../SkeletonLoader'
-import { TokenCarousel } from '../TokenCarousel'
+import Head from 'next/head'
+import React, { FC } from 'react'
+import { withLayout } from '../../common/layouts'
+import { Footer } from '../../modules/Footer'
+import { Photography } from '../../modules/Photography'
+import { SITE_DESCRIPTION, SITE_LOGO_PATH, SITE_TITLE, SITE_URL } from '../../common/constants'
+import { Layout, Network } from '../../common/types'
+import { useRouter } from 'next/router'
 
-export const GemsOnTheFloor: FC = () => {
-  const dispatch = useAppDispatch()
-  const network = Network.MAINNET
-
-  const { data: photographyFloors, status: photographyFloorsStatus } = useAppSelector(
-    selectCollectionFloorsByCollectionSetId({ collectionSetId: GEMS_ON_THE_FLOOR_PHOTOGRAPHY_COLLECTION_SET_ID }),
-  )
-
-  useEffect(() => {
-    if (GEMS_ON_THE_FLOOR_PHOTOGRAPHY_COLLECTION_SET_ID) {
-      dispatch(
-        collectionsApi.endpoints.getCollectionFloorsByCollectionSetId.initiate({
-          collectionSetId: GEMS_ON_THE_FLOOR_PHOTOGRAPHY_COLLECTION_SET_ID,
-        }),
-      )
-    }
-  }, [dispatch, network])
+const Floor: FC = () => {
+  const { asPath } = useRouter()
+  const siteTitle = `${SITE_TITLE} | Gems On The Floor`
+  const siteDescription = 'Gems On The Floor'
+  const url = `${SITE_URL}${asPath}`
 
   return (
-    <div className="w-full">
-      <div>
-        <h1 className="flex w-full pt-16 pb-8 text-6xl lg:text-8xl text-black justify-center text-center">
-          Photography
-        </h1>
-      </div>
-      <div className="w-full !text-black max-w-80 md:max-w-screen-md lg:max-w-screen-lg 2xl:max-w-screen-2xl mx-auto mt-3">
-        <h1 className="ml-3 text-4xl md:text-6xl lg:text-8xl text-black translate-y-10 md:translate-y-11">
-          Featured
-        </h1>
-        <div className="bg-white">
-          {equals(photographyFloorsStatus, QueryStatus.pending) ? (
-            <div className="grid grid-cols-4 gap-4 mb-8">
-              <SkeletonLoader height="h-8" style="light" />
-              <SkeletonLoader height="h-8" style="light" />
-              <SkeletonLoader height="h-8" style="light" />
-              <SkeletonLoader height="h-8" style="light" />
-            </div>
-          ) : (
-            photographyFloors && <TokenCarousel nfts={photographyFloors} network={network} />
-          )}
+    <div className="flex items-center flex-col">
+      <Head>
+        <title>{siteTitle}</title>
+        <meta name="description" content={siteDescription} />
+        <link rel="icon" href={SITE_LOGO_PATH} />
+
+        <meta name="title" content={siteTitle} />
+        <meta name="description" content={siteDescription} />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={siteTitle} />
+        <meta property="og:description" content={SITE_DESCRIPTION} />
+        <meta property="og:image" content={SITE_LOGO_PATH} />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content={SITE_LOGO_PATH} />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={siteTitle} />
+        <meta property="twitter:description" content={siteDescription} />
+        <meta property="twitter:image" content={SITE_LOGO_PATH} />
+      </Head>
+      <main className="w-full">
+        <div className="bg-white flex items-center justify-center">
+          <div className="">
+            <GemsOnTheFloor />
+          </div>
         </div>
-      </div>
+      </main>
     </div>
   )
 }
+
+export default withLayout(Layout.main)(Photography)
