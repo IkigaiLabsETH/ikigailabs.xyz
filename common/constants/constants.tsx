4f7ecf6b1b99fb83e544a59ef6462f3455eb722d
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2025-02-09 11:52:40 +0100

    Update constants.tsx
    remove all chain , just keep ethereum mainnet and berachain

--- common/constants/constants.tsx
+++ common/constants/constants.tsx
@@ -1,15 +1,3 @@
-import { defineChain } from 'thirdweb'
-import {
-  ethereum,
-  optimism,
-  polygon,
-  arbitrum,
-  arbitrumNova,
-  zora,
-  base,
-  baseSepolia,
-  avalanche,
-} from 'thirdweb/chains'
 import {
   FaArrowRightArrowLeft,
   FaSeedling,
@@ -20,8 +8,8 @@ import {
   FaRegCircleXmark,
 } from 'react-icons/fa6'
 import { ETH } from 'react-cryptoicon'
+import { mainnet } from 'wagmi/chains'
 import { Berachain } from '../config/chains'
-
 import { ActivityType, Network, Option } from '../types'
 
 export const COLLECTION_METADATA_FIELDS = ['name', 'description', 'image']
@@ -43,18 +31,6 @@ export const NETWORK_OPTIONS = [
   {
     id: 'ethereum',
     name: 'Ethereum',
-  },
-  {
-    id: 'polygon',
-    name: 'Polygon',
-  },
-  {
-    id: 'arbitrum',
-    name: 'Arbitrum',
-  },
-  {
-    id: 'mumbai',
-    name: 'Mumbai',
   },
   {
     id: 'berachain',
@@ -99,7 +75,7 @@ export const EXPIRATION_DEFAULTS = [
 
 export const customChains = {
   [Network.BERA]: {
-    id: 80094,
+    id: 80085,
     name: 'Berachain',
     network: 'berachain',
     nativeCurrency: {
@@ -108,118 +84,29 @@ export const customChains = {
       decimals: 18,
     },
     rpc: "https://rpc.berachain.com",
-  blockExplorers: [
-    {
-      name: "Berascan",
-      url: "https://berascan.com"
-    }
-  ],
+    blockExplorers: [
+      {
+        name: "Berascan",
+        url: "https://artio.beratrail.io"
+      }
+    ],
     testnet: false,
   },
-  [Network.LINEA]: defineChain({
-    id: 59144,
-    name: 'Linea',
-    nativeCurrency: {
-      name: 'Linea Ether',
-      symbol: 'ETH',
-      decimals: 18,
-    },
-    blockExplorers: [
-      {
-        name: 'Lineascan',
-        url: 'https://lineascan.build',
-      },
-      {
-        name: 'Blockscout',
-        url: 'https://explorer.linea.build',
-      },
-      {
-        name: 'L2scan',
-        url: 'https://linea.l2scan.co',
-      },
-    ],
-  }),
-  [Network.SCROLL]: defineChain({
-    id: 59145,
-    name: 'Scroll',
-    nativeCurrency: {
-      name: 'Scroll Ether',
-      symbol: 'ETH',
-      decimals: 18,
-    },
-    blockExplorers: [
-      {
-        name: 'Scrollscan',
-        url: 'https://scrollscan.build',
-      },
-    ],
-  }),
-  [Network.ZKSYNC]: defineChain({
-    id: 324,
-    name: 'ZkSync',
-    nativeCurrency: {
-      name: 'Ether',
-      symbol: 'ETH',
-      decimals: 18,
-    },
-    blockExplorers: [
-      {
-        name: 'zkSync Era Block Explorer',
-        url: 'https://explorer.zksync.io',
-      },
-    ],
-  }),
-  [Network.BLAST]: defineChain({
-    id: 59146,
-    name: 'Blast',
-    nativeCurrency: {
-      name: 'Blast Ether',
-      symbol: 'ETH',
-      decimals: 18,
-    },
-    blockExplorers: [
-      {
-        name: 'Blastscan',
-        url: 'https://blastscan.build',
-      },
-    ],
-  }),
-}
+} as const
 
 export const CHAIN_ID = {
-  [Network.MAINNET]: ethereum.id,
-  [Network.OPTIMISM]: optimism.id,
-  [Network.POLYGON]: polygon.id,
-  [Network.ARBITRUM]: arbitrum.id,
-  [Network.ARBITRUM_NOVA]: arbitrumNova.id,
-  [Network.ZORA]: zora.id,
-  [Network.BASE]: base.id,
-  [Network.BASE_SEPOLIA]: baseSepolia.id,
-  [Network.AVALANCHE]: avalanche.id,
+  [Network.MAINNET]: mainnet.id,
   [Network.BERA]: Berachain.id,
-  [Network.LINEA]: customChains[Network.LINEA].id,
-  [Network.ZKSYNC]: customChains[Network.ZKSYNC].id,
 } as const
 
 export const CHAINS = {
-  [Network.MAINNET]: ethereum,
-  [Network.OPTIMISM]: optimism,
-  [Network.POLYGON]: polygon,
-  [Network.ARBITRUM]: arbitrum,
-  [Network.ARBITRUM_NOVA]: arbitrumNova,
-  [Network.ZORA]: zora,
-  [Network.BASE]: base,
-  [Network.BASE_SEPOLIA]: baseSepolia,
-  [Network.AVALANCHE]: avalanche,
+  [Network.MAINNET]: mainnet,
   [Network.BERA]: Berachain,
-  [Network.LINEA]: customChains[Network.LINEA],
-  [Network.ZKSYNC]: customChains[Network.ZKSYNC],
 } as const
 
 export const CHAIN_ICON_MAP = {
   ETH: <ETH size={20} />,
   [Network.MAINNET]: <ETH size={20} />,
-  [Network.OPTIMISM]: <ETH size={20} />,
 }
 
 export const ACTIVITY_ICON_MAP = {
@@ -260,7 +147,7 @@ export const ACTIVITY_ICON_MAP = {
   ),
   [ActivityType.sale]: (
     <div className="h-4 w-4 mr-2">
-      <FaCartShopping className="mr-1 !h-4 !w-4 text-neutral-400 md:mr-[10px] md:h-5 md:w-5" />,
+      <FaCartShopping className="mr-1 !h-4 !w-4 text-neutral-400 md:mr-[10px] md:h-5 md:w-5" />
     </div>
   ),
 }
