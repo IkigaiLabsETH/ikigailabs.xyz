2aa721a1715dc233c76c358c0763c1b73cf7a710
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2025-02-09 12:03:34 +0100

    Update useWallet.ts

--- common/useWallet/useWallet.ts
+++ common/useWallet/useWallet.ts
@@ -1,12 +1,15 @@
 import { useActiveAccount, useActiveWalletChain } from 'thirdweb/react'
-import { TW_SUPPORTED_CHAINS } from '../config/chains'
+import { supportedChains } from '../config/chains'
 import { propEq } from 'ramda'
 
 export const useWallet = () => {
   const account = useActiveAccount()
   const networkId = useActiveWalletChain()
-  const network = TW_SUPPORTED_CHAINS.find(propEq('id', networkId))
+  const network = supportedChains.find(propEq('id', networkId))
   const address = account?.address || ''
 
-  return { address, network: network?.slug }
+  return { 
+    address, 
+    network: network?.network // Changed from slug to network since that's what we have in ReservoirChain
+  }
 }
