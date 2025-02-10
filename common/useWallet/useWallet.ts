f90e42c364723073b319003f5240a894c7afb387
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2025-02-10 15:12:29 +0100

    Update useWallet.ts

--- common/useWallet/useWallet.ts
+++ common/useWallet/useWallet.ts
@@ -1,21 +1,21 @@
 import { 
-  useActiveAccount,
-  useActiveWalletChain,
+  useWallet as useThirdwebWallet,
   useActiveWalletConnectionStatus 
 } from 'thirdweb/react'
 import { supportedChains } from '../constants/constants'
 import { ReservoirChain } from '../types'
 
 export const useWallet = () => {
-  const account = useActiveAccount()
-  const chain = useActiveWalletChain()
+  const wallet = useThirdwebWallet()
   const connectionStatus = useActiveWalletConnectionStatus()
-  const network = supportedChains.find((c: ReservoirChain) => c.id === chain?.id)
+  const network = supportedChains.find((c: ReservoirChain) => c.id === wallet?.chain?.id)
 
   return { 
-    address: account?.address,
+    wallet,
+    connectionStatus,
+    address: wallet?.address,
     network,
-    networkId: chain?.id,
+    networkId: wallet?.chain?.id,
     isConnected: connectionStatus === "connected"
   }
 }
