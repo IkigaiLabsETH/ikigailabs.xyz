643dcd9fe01caf1287c107c0bc028d268fb910b3
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2025-02-10 11:38:15 +0100

    Update chains.ts

--- common/config/chains.ts
+++ common/config/chains.ts
@@ -1,4 +1,4 @@
-import { mainnet } from 'wagmi/chains'
+import { mainnet, base, arbitrum } from 'wagmi/chains'
 import { Network } from '../types'
 
 // Base chain type for Thirdweb
@@ -84,7 +84,55 @@ export const Berachain: ReservoirChain = {
   routePrefix: Network.BERA,
 } as const;
 
+export const Base: ReservoirChain = {
+  id: base.id,
+  name: "Base",
+  network: "base",
+  rpc: [base.rpcUrls.default.http[0]],
+  nativeCurrency: base.nativeCurrency,
+  blockExplorers: [
+    {
+      name: "BaseScan",
+      url: base.blockExplorers.default.url
+    }
+  ],
+  lightIconUrl: '/icons/base-icon-dark.svg',
+  darkIconUrl: '/icons/base-icon-light.svg',
+  reservoirBaseUrl: 'https://api-base.reservoir.tools',
+  proxyApi: '/api/reservoir/base',
+  routePrefix: Network.BASE,
+  apiKey: process.env.BASE_RESERVOIR_API_KEY,
+  coingeckoId: 'base',
+  collectionSetId: process.env.NEXT_PUBLIC_BASE_COLLECTION_SET_ID,
+  community: process.env.NEXT_PUBLIC_BASE_COMMUNITY,
+} as const
+
+export const Arbitrum: ReservoirChain = {
+  id: arbitrum.id,
+  name: "Arbitrum",
+  network: "arbitrum",
+  rpc: [arbitrum.rpcUrls.default.http[0]],
+  nativeCurrency: arbitrum.nativeCurrency,
+  blockExplorers: [
+    {
+      name: "ArbiScan",
+      url: arbitrum.blockExplorers.default.url
+    }
+  ],
+  lightIconUrl: '/icons/arbitrum-icon-dark.svg',
+  darkIconUrl: '/icons/arbitrum-icon-light.svg',
+  reservoirBaseUrl: 'https://api-arbitrum.reservoir.tools',
+  proxyApi: '/api/reservoir/arbitrum',
+  routePrefix: Network.ARBITRUM,
+  apiKey: process.env.ARBITRUM_RESERVOIR_API_KEY,
+  coingeckoId: 'arbitrum',
+  collectionSetId: process.env.NEXT_PUBLIC_ARBITRUM_COLLECTION_SET_ID,
+  community: process.env.NEXT_PUBLIC_ARBITRUM_COMMUNITY,
+} as const
+
 export const supportedChains = [
   defaultChain,
+  Base,
+  Arbitrum,
   Berachain,
 ] as ReservoirChain[]
