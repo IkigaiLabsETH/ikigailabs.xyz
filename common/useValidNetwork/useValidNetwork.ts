169ea9fce39f7bd99bce22e827cd37977cff2ae5
IKIGAI LABS XYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2025-02-10 12:25:25 +0100

    Update useValidNetwork.ts

--- common/useValidNetwork/useValidNetwork.ts
+++ common/useValidNetwork/useValidNetwork.ts
@@ -4,15 +4,8 @@ import { Network } from '../types'
 export const useValidNetwork = (network: string) => {
   const supportedNetworks = [
     Network.MAINNET,
-    Network.OPTIMISM,
-    Network.POLYGON,
     Network.ARBITRUM,
-    Network.MUMBAI,
-    Network.ZORA,
     Network.BASE,
-    Network.BLAST,
-    Network.SCROLL,
-    Network.SEPOLIA,
   ]
 
   return includes(network, supportedNetworks)
